ebefedaa9e3fd340f392666ac61595e123a9a9b9
EmoN-Fang <fanghzh@shanghaitech.edu.cn>
2018-03-17 21:46:43 +0800

    simply try

--- Project1_Part_A_search/search.py
+++ Project1_Part_A_search/search.py
@@ -87,6 +87,8 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
+
+    
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
